a31138b20dc33df9fac05d58bae867f41505d45b
Tim <goodwin729@yandex.ru>
2015-10-31 09:15:05 +0300

    change dir and add 2nd

---
+++ split/js/scripts.js
@@ -0,0 +1,27 @@
+
+
+$('#modalVideo').on('man__trigger', function () {
+    $('#modalVideo').focus()
+});
+$('#modalTariff').on('footer__link', function () {
+    $('#modalTariff').focus()
+});
+$('#modalAgreement').on('checkbox__label', function () {
+    $('#modalAgreement').focus()
+});
+/*----------------
+ scroll
+ ----------------*/
+$(window).load(function(){
+    $(".getScroll").mCustomScrollbar({
+    });
+});
+
+
+
+/*$( ".checkbox" ).click(function() {
+    $( '.checkbox' ).toggleClass( "active" );
+});*/
+
+
+
